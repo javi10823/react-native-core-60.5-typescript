6d4d21f6471ca4f31ac156911fb739ed69578d9d
LcsGrz <lucas.gerez@gmail.com>
2019-08-21 23:28:16 -0300

    app icons

--- android/app/src/main/java/com/coremobile/generated/BasePackageList.java
+++ android/app/src/main/java/com/coremobile/generated/BasePackageList.java
@@ -1,16 +1,15 @@
 package com.coremobile.generated;
 
-import org.unimodules.core.interfaces.Package;
-
 import java.util.Arrays;
 import java.util.List;
+import org.unimodules.core.interfaces.Package;
 
 public class BasePackageList {
-    public List<Package> getPackageList() {
-        return Arrays.<Package>asList(
-                new expo.modules.constants.ConstantsPackage(),
-                new expo.modules.filesystem.FileSystemPackage(),
-                new expo.modules.permissions.PermissionsPackage()
-        );
-    }
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
 }
